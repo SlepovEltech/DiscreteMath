6853de247648aa7609281e2ad382ad4806a9fbf5
Vova <vovavelho@gmail.com>
2019-03-06 23:54:36 +0300

    added basic polynom constructor (that not working yet)

---
+++ Sandbox/smt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "../struct.h"
+#include "lims.h"
+
+
+P* initPolynom(int power){
+// PAIN
+int i;
+P* output = (P*)malloc(sizeof(P));
+output->c = (Q*)malloc(power*sizeof(Q));
+output->m = power;
+for(i=0;i<m;i++)
+    
+}
+
+
+int main(){
+
+
+    return 0;
+}
