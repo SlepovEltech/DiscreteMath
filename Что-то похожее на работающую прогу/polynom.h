c0380816a5f12f374dacfe15ca872626f3b96fb6
Vova <vovavelho@gmail.com>
2019-03-20 22:24:51 +0300

    артем памаги

--- Что-то похожее на работающую прогу/polynom.h
+++ Что-то похожее на работающую прогу/polynom.h
@@ -11,31 +11,44 @@ P* ADD_PP_P(P* first,P* second){
     int maxDeg = ((first->deg>second->deg)?first->deg:second->deg);
     P* result = (P*)calloc(1,sizeof(P));
     Q* tmp;
+    Q zero;
+    int nu[1] = {0};
+    int de[1] = {1};
+    N a,b;
+    Z c;
+    a.n = 1;
+    a.A = nu;
+    c.sign = 1;
+    c.num = &a;
+    b.n = 1;
+    b.A = de;
+    zero.m = &c;
+    zero.n = &b;
+
     result->deg = maxDeg;
     result->c = (Q*)calloc(maxDeg+1,sizeof(Q));
     for(i=0;i<maxDeg;i++){
+        
         if((i<first->deg)&&(i<second->deg)){
-            printf("A\n");
             tmp = ADD_QQ_Q(&(first->c[i]),&(second->c[i]));
             result->c[i] = *tmp;
             free(tmp);
         }
-        else
-            if((i<first->deg)){
-            printf("B\n");
-            tmp = ADD_QQ_Q(&(first->c[i]),&(result->c[i]));
-            result->c[i] = *tmp;
-            free(tmp);
+        else{
+                if((i<first->deg))
+                {
+                    tmp = ADD_QQ_Q(&(first->c[i]),&zero);
+                    result->c[i] = *tmp;
+                    free(tmp);
+                }                
+                else
+                    if((i<second->deg))
+                    {
+                        tmp = ADD_QQ_Q(&(second->c[i]),&zero);
+                        result->c[i] = *tmp;
+                        free(tmp);
+                    }
         }
-                    
-            else
-                if((i<second->deg)){
-            printf("C\n");
-            tmp = ADD_QQ_Q(&(second->c[i]),&(result->c[i]));
-            result->c[i] = *tmp;
-            free(tmp);
-        }
-        printf("%d\n",i);
     }
     return result;
 }
